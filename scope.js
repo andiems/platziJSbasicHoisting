85ac2668ac77b03f48ccdb2b3a2c5037748204bf
andiems <andreasantillandev@gmail.com>
2024-03-25 22:27:38 -0600

    Clase que explica los Scope, global y  local

---
+++ scope.js
@@ -0,0 +1,11 @@
+//codigo ejecutado en la consola que explica el scope global y local
+
+
+var miNombre = "Andrea";
+
+function nombre(){
+    var miApellido = "Santillan";
+    console.log(miNombre + " " + miApellido);
+}
+
+nombre();
