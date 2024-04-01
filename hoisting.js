788ab0215654a72ca2608175ba955acc67990ba4
andiems <andreasantillandev@gmail.com>
2024-03-31 21:42:05 -0600

    clase de buenas practicas para prevenir el hoisting, se corre el código en la consola del navegador

---
+++ hoisting.js
@@ -0,0 +1,16 @@
+var miNombre; //declara una variable
+
+miNombre = "Andrea"; //inicializa una variable;
+
+var miSegundoName = "Marlene"; //declara e inicializa una variable
+
+
+
+
+hey(); //correr en el navegador para probar como funciona el hoisting
+
+function hey(){
+    console.log("hola " + miNombre);
+}
+
+var miNombre ="Andrea";
